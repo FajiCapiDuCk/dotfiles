1313e66ed51d262b7348c033bd444042e01d6d5c
FajiCapiDuCk <fajicapiduck@gmail.com>
2026-08-25 11:25:58 +0200

    Added system related paths to variables.zsh

--- .zsh/variables.zsh
+++ .zsh/variables.zsh
@@ -1,12 +1,17 @@
-#Haskell binaries
+# Haskell binaries
 if [[ -d ~/.ghcup/bin ]]; then
 export PATH="$HOME/.ghcup/bin:$PATH"
 fi
-#Rust custom installed stuff
+# Rust custom installed stuff
 if [[ -d ~/.cargo/bin ]]; then
 export PATH="$HOME/.cargo/bin:$PATH"
 fi
-#Golang binary path
+# Golang binary path
 if [[ -d ~/go/bin ]]; then
 export PATH="$HOME/go/bin:$PATH"
 fi
+# System related binaries adding to path
+export PATH="/usr/sbin:$PATH"
+export PATH="/usr/bin:$PATH"
+export PATH="/usr/local/bin:$PATH"
+export PATH="/usr/local/sbin:$PATH"
